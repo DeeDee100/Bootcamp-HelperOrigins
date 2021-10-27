0b0135f1f1d7b967896f63050513cab81fe5430c
DeeDee <amanda_tinoco00@hotmail.com>
2021-10-27 17:55:48 -0300

    Projeto finalizado

---
+++ Sprint1/Exercicio1/src/ex1.js
@@ -0,0 +1,14 @@
+function number_vogals() {
+    var box = document.getElementById("texto");
+    var phrase = box.value;
+    var count = 0;
+    phrase = phrase.toLowerCase();
+    for (var _i = 0, phrase_1 = phrase; _i < phrase_1.length; _i++) {
+        var char = phrase_1[_i];
+        if (char == 'a' || char == 'e' || char == 'i' || char == 'o' || char == 'u') {
+            count++;
+        }
+    }
+    alert(count);
+}
+//# sourceMappingURL=ex1.js.map
