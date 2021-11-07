65be4a7a3cfc218ec6d2dad2242080282d486754
DeeDee <amanda_tinoco00@hotmail.com>
2021-11-07 02:10:51 -0300

    exclusao Ex5

--- Sprint2/Exercicio5/src/ex5_Formulario.js
+++
@@ -1,9 +0,0 @@
-import { Vogals } from "../ex5_Funcao.js";
-function mouseClick() {
-    let texto = document.getElementById("text").value;
-    let frase = new Vogals(texto);
-    alert(frase.getVogals());
-}
-const btn = document.getElementById("btn");
-btn.onclick = mouseClick;
-//# sourceMappingURL=ex5_Formulario.js.map
