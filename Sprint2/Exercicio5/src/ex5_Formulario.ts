211a80ba1db127bdfe0348d064d72b0ca9610e1f
DeeDee <amanda_tinoco00@hotmail.com>
2021-11-08 23:15:48 -0300

    fix: let pra const |  troca btn para button

--- Sprint2/Exercicio5/src/ex5_Formulario.ts
+++ Sprint2/Exercicio5/src/ex5_Formulario.ts
@@ -1,10 +1,10 @@
 import { Vogals } from "../ex5_Funcao.js"
 
 function mouseClick(): void{
-	let texto: string = (document.getElementById("text") as HTMLInputElement).value;
-	let frase: Vogals = new Vogals(texto);
+	const texto: string = (document.getElementById("text") as HTMLInputElement).value;
+	const frase: Vogals = new Vogals(texto);
 	alert(frase.getVogals());
 }
 
-const btn = document.getElementById("btn");
-btn.onclick = mouseClick;
+const button = document.getElementById("button");
+button.onclick = mouseClick;
