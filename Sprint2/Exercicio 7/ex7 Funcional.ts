a426b30ebf89859eb893516a0b976ea6e6f62f62
DeeDee <amanda_tinoco00@hotmail.com>
2021-11-10 15:04:16 -0300

    fix: add pasta exercicio 7

---
+++ Sprint2/Exercicio 7/ex7 Funcional.ts
@@ -0,0 +1,45 @@
+const numberList = [1,2,6,9,4];
+const MixedList = [1,2,6,"lul", 9];
+
+class Values{
+	list: Array<any>;
+
+	constructor(list: Array<any>){
+		this.list = [...list];
+	}
+
+	maxValue(){
+		let newList: Array<number> = [...this.list];
+		newList.sort();
+		return newList.pop() || 0;
+	}
+
+	minValue(){
+		let newList: Array<number> = [...this.list];
+		newList.sort();
+		return newList.shift() || 0;
+	}
+
+	average(){
+	let newList: Array<number> = [...this.list];
+	let sum: number = newList.reduce((numero1, numero2) => numero1 + numero2, 0);
+	const result: number = sum / (newList.length || 1);
+	return result;
+	}
+
+	maxMinAverage(){
+		if (this.list.length === 0){
+			return "Lista Vazia";
+		}
+		else if (!this.list.every(item => typeof(item) === "number")){
+			return "Item não é um número";
+		}
+		else{
+			const big: number = this.maxValue();
+			const minimun: number = this.minValue();
+			const average: number = this.average();
+
+			return [big, minimun, average];
+		}
+	}
+}
--- Sprint2/ex7 Funcional.ts
+++
@@ -1,3 +0,0 @@
-const numberList = [1,2,6,9,4];
-const MixedList = [1,2,6,"lul", 9];
-
